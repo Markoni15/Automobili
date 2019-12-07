cdde5ab2e8189a27f48a3564e52051281289050b
Markoni15 <47751660+Markoni15@users.noreply.github.com>
2019-12-07 19:18:04 +0100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix">
+	<a href="dashboard.php" style="font-size: 20px;">Portal za rentiranje auta | Admin Panel</a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Administrator <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Promeni šifru</a></li>
+					<li><a href="logout.php">Izloguj se</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ includes/leftbar.php
@@ -0,0 +1,30 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Glavno</li>
+				
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brendovi</a>
+<ul>
+<li><a href="create-brand.php">Kreiraj brend</a></li>
+<li><a href="manage-brands.php">Izmena brendova</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Automobili</a>
+					<ul>
+						<li><a href="post-avehical.php">Postavi vozilo</a></li>
+						<li><a href="manage-vehicles.php">Izmena vozila</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Menadžment bukinga</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Menadžment utisaka</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Menadžment pitanja</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Registrovani korisnici</a></li>
+			
+
+			
+
+			</ul>
+		</nav>
